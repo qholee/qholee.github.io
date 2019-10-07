6465bb56c92d954b8cfdca1749b5b0929c04a7d4
Kyu Ho Lee <39530850+qholee@users.noreply.github.com>
2019-10-06 21:05:40 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#menu li a").on('click', function() {
-        var page = $(this).data('page');
-        $("#pages .page:not('.hide')").stop().fadeOut('fast', function() {
-            $(this).addClass('hide');
-            $('#pages .page[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
-        });
-    });
-});
